2144324127e7c22b590266808ab04ddfd4fa4fa8
Ekta <ektaverma@outlook.com>
2025-04-19 00:45:03 +0530

    Update: [added scroll trigger and gsap function]

---
+++ script.js
@@ -0,0 +1,30 @@
+var cursor = document.querySelector('#cursor')
+document.addEventListener("mousemove",function(dets){
+cursor.style.left = dets.x+"px";
+cursor.style.top = dets.y+"px"
+
+})
+gsap.to("#nav",{
+   backgroundColor : "black",
+   height:"90px",
+   duration:0.5,
+   scrollTrigger:{
+    trigger:"#nav", // kya trigger hoga 
+    scroller:"body", // kya scroll krne pr trigger hoga
+   // markers:true,
+    start: "top -10%",// kb trigger strt hoga
+    end: "top -11%", // kb end hoga 
+    scrub: 1 //smooth scrolling ke lie
+   }
+})
+gsap.to("#main",{
+backgroundColor:"#000",
+scrollTrigger:{
+    trigger:"#main",
+    scroller:"body",
+    //markers:true,
+    start:"top -25%",
+    end: "top -70%",
+    scrub:1
+}
+})
